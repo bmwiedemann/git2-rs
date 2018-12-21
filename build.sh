09d564de6d34f648a530ed4a0c3dfd793ec514fe
Bernhard M. Wiedemann <bwiedemann@suse.de>
2018-12-21 14:44:23 +0100

    reduce initial build

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ opt="-C opt-level=2"
 rustc src/lib.rs --crate-type lib $opt --emit=asm \
 -A dead_code -A missing_docs -A unused_imports -A unused \
 -L dependency=$deps \
---extern libc=$deps/liblibc-0b3d13cd53349b87.rlib \
---extern libgit2_sys=$deps/liblibgit2_sys-4e85a42dd8d5350f.rlib \
+--extern libc=$(echo $deps/liblibc-*.rlib) \
+--extern libgit2_sys=$(echo $deps/liblibgit2_sys-*.rlib) \
 
 md5sum lib.s
